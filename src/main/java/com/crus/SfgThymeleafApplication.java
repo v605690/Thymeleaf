8f54ebfb0ea61a3a9e8ee75ba72209985692e3bc
v605690 <v605690@gmail.com>
2025-11-26 08:24:39 -0600

    add thymeleaf project

---
+++ src/main/java/com/crus/SfgThymeleafApplication.java
@@ -0,0 +1,13 @@
+package com.crus;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SfgThymeleafApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SfgThymeleafApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/crus/SfgThymeleafApplicationTests.java
@@ -0,0 +1,13 @@
+package com.crus;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SfgThymeleafApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
